cdc951a2033c73567dda3ed9e5d9a750ed6954c8
C. Petit <christ.petit@gmail.com>
2021-03-20 12:20:49 +0100

    v3 use odk tooling to request write rights befoire injectingg forms

--- collect_app/src/main/java/atlaos/AtlaosInitActivity.java
+++ collect_app/src/main/java/atlaos/AtlaosInitActivity.java
@@ -1,19 +1,21 @@
 package atlaos;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.view.Window;
 
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
 
+import org.odk.collect.android.R;
 import org.odk.collect.android.activities.SplashScreenActivity;
+import org.odk.collect.android.listeners.PermissionListener;
 import org.odk.collect.android.storage.StoragePathProvider;
 import org.odk.collect.android.storage.StorageSubdirectory;
+import org.odk.collect.android.utilities.PermissionUtils;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -21,10 +23,33 @@ import java.io.InputStream;
 
 import timber.log.Timber;
 
+
 public class AtlaosInitActivity extends Activity {
+
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        PermissionUtils permissionUtils = new PermissionUtils(R.style.Theme_Collect_Dialog_PermissionAlert);
+        permissionUtils.requestStoragePermissions(this, new PermissionListener() {
+            @Override
+            public void granted() {
+                // must be at the beginning of any activity that can be called from an external intent
+
+                launch();
+            }
+
+            @Override
+            public void denied() {
+                // The activity has to finish because ODK Collect cannot function without these permissions.
+                finish();
+            }
+        });
+
+    }
+
+    private void launch() {
         try {
             PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
             String version = pInfo.versionName;
@@ -46,21 +71,19 @@ public class AtlaosInitActivity extends Activity {
 
     private void reinitforms() throws Exception {
         //
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
         StoragePathProvider spp = new StoragePathProvider();
         File formsDir = new File(spp.getDirPath(StorageSubdirectory.FORMS));
         if (!formsDir.exists()) {
             formsDir.mkdirs();
         }
-        //InputStream in = getResources().openRawResource(R.raw.atlas);
         for (String form : getAssets().list("forms")) {
             Timber.d("found :" + form);
             String formPath = "forms" + File.separator + form;
             Timber.d("importing form :" + formPath);
             InputStream in = getAssets().open(formPath);
             FileOutputStream out = new FileOutputStream(new File(formsDir, form));
-            byte[] buff = new byte[1024];
+            byte[] buff = new byte[10240];
             int read = 0;
 
             try {
@@ -73,5 +96,6 @@ public class AtlaosInitActivity extends Activity {
             }
         }
     }
-    }
 
+
+}
